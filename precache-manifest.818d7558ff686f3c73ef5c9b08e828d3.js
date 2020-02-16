2f47f3d1a1fb8b530bcf449ab8adf0a05acc93ec
heardman <markyheard@gmail.com>
2020-02-15 21:27:06 -0800

    Updates

---
+++ precache-manifest.818d7558ff686f3c73ef5c9b08e828d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e73d9924f0010c51cf944170f14fd79",
+    "url": "/markheardio-admin-dashboard/index.html"
+  },
+  {
+    "revision": "65a86b703955bb843723",
+    "url": "/markheardio-admin-dashboard/static/css/main.b582cef1.chunk.css"
+  },
+  {
+    "revision": "aa0a440c9fc2610968d8",
+    "url": "/markheardio-admin-dashboard/static/js/2.f73e7c90.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/markheardio-admin-dashboard/static/js/2.f73e7c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a86b703955bb843723",
+    "url": "/markheardio-admin-dashboard/static/js/main.9674ed36.chunk.js"
+  },
+  {
+    "revision": "2522a1960074a8d3d625",
+    "url": "/markheardio-admin-dashboard/static/js/runtime-main.f0679de9.js"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/markheardio-admin-dashboard/static/media/close-dark.777b93c6.svg"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/markheardio-admin-dashboard/static/media/menu-dark.c275e518.svg"
+  }
+]);
