883c568901f3bbf0ce6ccae40e783cecd4371d94
heardman <markyheard@gmail.com>
2020-02-16 15:53:33 -0800

    adjusting deployment

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -25,7 +25,7 @@ const NavBar = () => {
       return <a hreh='#' className='menu-link' onClick={() => logout()}>Log out</a>;
     } else if (isAuthenticated === false) {
       const options = {redirect_uri: "https://heardman.github.io/markheardio-admin-dashboard/"}
-      return <a href='#' className='menu-link' onClick={() => loginWithRedirect(options)}>Log in</a>;
+      return <a href='#' className='menu-link' onClick={() => loginWithRedirect()}>Log in</a>;
     }
   }
 
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const onRedirectCallback = appState => {
   history.push(
     appState && appState.targetUrl
       ? appState.targetUrl
-      : "https://heardman.github.io/markheardio-admin-dashboard/"
+      : window.location.pathname
   );
 };
 
@@ -20,7 +20,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={config.domain}
     client_id={config.clientId}
-    redirect_uri={"https://heardman.github.io/markheardio-admin-dashboard/"}
+    redirect_uri={window.location.origin}
     audience={config.audience}
     onRedirectCallback={onRedirectCallback}
   >
