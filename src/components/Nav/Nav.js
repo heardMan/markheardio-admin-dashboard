44bfbca4113d57ed90f444c5cb85f9221dfa21ed
heardman <markyheard@gmail.com>
2020-02-16 16:03:41 -0800

    adjusting deployment

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -24,8 +24,10 @@ const NavBar = () => {
     if (isAuthenticated) {
       return <a hreh='#' className='menu-link' onClick={() => logout()}>Log out</a>;
     } else if (isAuthenticated === false) {
-      const options = {redirect_uri: "https://heardman.github.io/markheardio-admin-dashboard/"}
-      return <a href='#' className='menu-link' onClick={() => loginWithRedirect()}>Log in</a>;
+      const options = {auth: {
+        redirectUrl: 'https://heardman.github.io/markheardio-admin-dashboard/'
+      }}
+      return <a href='#' className='menu-link' onClick={() => loginWithRedirect(options)}>Log in</a>;
     }
   }
 
